bba8abbee2be9bd07ccc27c134d4423aa93b0dfa
Divya Rani <91890152+RDivya01@users.noreply.github.com>
2024-08-06 20:27:18 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -17,6 +17,7 @@ int main() {
     assert(size(37) == 'S');
     assert(size(38) == 'M');
     assert(size(40) == 'M');
+    assert(size(42) == 'L');
     assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
     return 0;
